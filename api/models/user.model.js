97af5b0050422a9ac5064566ed31e0813956935b
abhijith888 <abhijithsangars@gmail.com>
2024-08-26 15:59:12 +0530

    create signup api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,19 +1,18 @@
 import mongoose from "mongoose";
-import { type } from "os";
 
 const userSchema = new mongoose.Schema({
   username : {
-    type: string ,
+    type: String ,
     required: true , 
     unique: true ,
   },
   email : {
-    type: string ,
+    type: String ,
     required: true , 
     unique: true ,
   },
   password:{
-    type: string , 
+    type: String , 
     required: true ,
   },
 } , {timestamps: true}
